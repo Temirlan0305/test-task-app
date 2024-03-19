7a851859c951bfcbb6a0ddcfcdcba6a1ce68c3a6
Temirlan Kanafin <timok.kanafin@gmail.com>
2024-03-20 02:43:57 +0500

    [ADD]

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Layout } from './components/layout'
 import Note from './pages/Note'
 import NoteCreate from './pages/NoteCreate'
 import NoteView from './pages/NoteView'
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Note />,
-	},
-	{
-		path: '/notes/create',
-		element: <NoteCreate />,
-	},
-	{
-		path: '/notes/:id',
-		element: <NoteView />,
-	},
-])
 function App() {
-	return <>qwdqwd</>
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route path='' element={<Note />} />
+				<Route path='notes/create' element={<NoteCreate />} />
+				<Route path='notes/:id' element={<NoteView />} />
+			</Route>
+		</Routes>
+	)
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import App from './App'
@@ -20,20 +21,22 @@ const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<Provider store={store}>
-				<App />
-				<ToastContainer
-					position='top-right'
-					autoClose={5000}
-					newestOnTop={false}
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					theme='light'
-				/>
-			</Provider>
-		</QueryClientProvider>
+		<BrowserRouter>
+			<QueryClientProvider client={queryClient}>
+				<Provider store={store}>
+					<App />
+					<ToastContainer
+						position='top-right'
+						autoClose={5000}
+						newestOnTop={false}
+						closeOnClick
+						rtl={false}
+						pauseOnFocusLoss
+						draggable
+						theme='light'
+					/>
+				</Provider>
+			</QueryClientProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 )
