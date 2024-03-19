0f0e94787fca17b468f9c34334b5dd4b99178d15
Temirlan Kanafin <timok.kanafin@gmail.com>
2024-03-20 02:48:04 +0500

    [ADD]

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import App from './App'
 import './index.css'
-import { store } from './redux/store'
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -20,12 +16,10 @@ const queryClient = new QueryClient({
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<Provider store={store}>
-					<App />
-					<ToastContainer
+	<BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<App />
+			{/* <ToastContainer
 						position='top-right'
 						autoClose={5000}
 						newestOnTop={false}
@@ -34,9 +28,7 @@ root.render(
 						pauseOnFocusLoss
 						draggable
 						theme='light'
-					/>
-				</Provider>
-			</QueryClientProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+					/> */}
+		</QueryClientProvider>
+	</BrowserRouter>
 )
