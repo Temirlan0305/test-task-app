d31779ceb4eeeebe35d9c23e6a06b43ab8ab2e93
Temirlan Kanafin <timok.kanafin@gmail.com>
2024-03-20 02:28:05 +0500

    [ADD]

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
-import { Layout } from './components/layout'
 import Note from './pages/Note'
 import NoteCreate from './pages/NoteCreate'
 import NoteView from './pages/NoteView'
 
 const router = createBrowserRouter([
 	{
-		element: <Layout />,
 		path: '/',
-		children: [
-			{
-				path: '/',
-				element: <Note />,
-			},
-			{
-				path: '/notes/create',
-				element: <NoteCreate />,
-			},
-			{
-				path: '/notes/:id',
-				element: <NoteView />,
-			},
-		],
+		element: <Note />,
+	},
+	{
+		path: '/notes/create',
+		element: <NoteCreate />,
+	},
+	{
+		path: '/notes/:id',
+		element: <NoteView />,
 	},
 ])
 function App() {
-	return <RouterProvider router={router} />
+	return <>qwdqwd</>
 }
 
 export default App
