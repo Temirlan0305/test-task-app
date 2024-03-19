f63bcd233ce076c19e45adf3e8d73ddedbe8c850
Temirlan Kanafin <timok.kanafin@gmail.com>
2024-03-20 03:15:50 +0500

    [ADD]

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import App from './App'
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root')!)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
