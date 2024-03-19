db07054024860c35fdf40120d2078d35e2390f9c
Temirlan Kanafin <timok.kanafin@gmail.com>
2024-03-19 21:30:02 +0500

    [ADD]

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "https://mockapi.io/projects/65c953efa4fbc162e112c722", // путь
-                changeOrigin: true,
-            },
-        },
-    },
-    assetsInclude: ["**/*.docx", "**/*.doc", "**/*.pptx", "**/*.pdf"],
-});
+	base: '/test-task-app/',
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://mockapi.io/projects/65c953efa4fbc162e112c722', // путь
+				changeOrigin: true,
+			},
+		},
+	},
+	assetsInclude: ['**/*.docx', '**/*.doc', '**/*.pptx', '**/*.pdf'],
+})
