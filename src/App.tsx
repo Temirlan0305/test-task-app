91f6ab0f05dc17bfe625d1cac738ec57bbe6ecfc
Temirlan Kanafin <timok.kanafin@gmail.com>
2024-03-20 03:01:49 +0500

    [ADD]

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Layout } from './components/layout'
 import Note from './pages/Note'
 import NoteCreate from './pages/NoteCreate'
 import NoteView from './pages/NoteView'
@@ -9,11 +8,9 @@ import NoteView from './pages/NoteView'
 const App: FC = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<Layout />}>
-				<Route path='' element={<Note />} />
-				<Route path='notes/create' element={<NoteCreate />} />
-				<Route path='notes/:id' element={<NoteView />} />
-			</Route>
+			<Route path='/' element={<Note />} />
+			<Route path='/notes/create' element={<NoteCreate />} />
+			<Route path='/notes/:id' element={<NoteView />} />
 		</Routes>
 	)
 }
